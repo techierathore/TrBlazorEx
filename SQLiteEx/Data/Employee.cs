1e261d8a83d35d6b711811a0054f6d4cfc48381d
S Ravi Kumar <techierathore@gmail.com>
2020-01-16 16:52:58 +0530

    Added SQLiteEx code which is an example of using SQLite in Blazor with Frank's DLL

---
+++ SQLiteEx/Data/Employee.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace SQLiteEx.Data
+{
+    public class Employee
+    {
+        [PrimaryKey, AutoIncrement]
+        public long EmpId
+        { get; set; }
+        [NotNull]
+        public string EmpName
+        { get; set; }
+        public string Designation
+        { get; set; }
+        public string Department
+        { get; set; }
+        public string Qualification
+        { get; set; }
+    }
+}

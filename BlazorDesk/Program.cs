55b8c87c261f94a076cb1d348a8e728b02c40cc6
S Ravi Kumar <techierathore@gmail.com>
2020-10-15 22:22:48 +0530

    Updated the Desktop Version to use Chromely

--- BlazorDesk/Program.cs
+++ BlazorDesk/Program.cs
@@ -1,12 +1,90 @@
-﻿using WebWindows.Blazor;
+﻿using Chromely;
+using System.Threading;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Chromely.Core.Configuration;
+using Chromely.Core;
+using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
 
 namespace BlazorDesk
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            ComponentsDesktop.Run<Startup>("My Blazor App", "wwwroot/index.html");
+            var appName = Assembly.GetEntryAssembly()?.GetName().Name;
+            var firstProcess = ServerAppUtil.IsMainProcess(args);
+            var port = ServerAppUtil.AvailablePort;
+
+            if (firstProcess)
+            {
+                if (port != -1)
+                {
+                    // start the kestrel server in a background thread
+                    var blazorTask = new Task(() => CreateHostBuilder(args, port).Build().Run(), TaskCreationOptions.LongRunning);
+                    blazorTask.Start();
+
+                    // wait till its up
+                    while (ServerAppUtil.IsPortAvailable(port))
+                    {
+                        Thread.Sleep(1);
+                    }
+                }
+
+                // Save port for later use by chromely processes
+                ServerAppUtil.SavePort(appName, port);
+            }
+            else
+            {
+                // fetch port number
+                port = ServerAppUtil.GetSavedPort(appName);
+            }
+
+            if (port != -1)
+            {
+                // start up chromely
+                var core = typeof(IChromelyConfiguration).Assembly;
+                var config = DefaultConfiguration.CreateForRuntimePlatform();
+                config.WindowOptions.Title = "blazor server app demo";
+                config.StartUrl = $"https://127.0.0.1:{port}";
+                config.DebuggingMode = true;
+                config.WindowOptions.RelativePathToIconFile = "chromely.ico";
+
+                try
+                {
+                    var builder = AppBuilder.Create();
+                    builder = builder.UseConfig<DefaultConfiguration>(config);
+                    builder = builder.UseApp<DemoChromelyApp>();
+                    builder = builder.Build();
+                    builder.Run(args);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    throw;
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args, int port) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseUrls(new[] { $"https://127.0.0.1:{port}" });
+                });
+    }
+
+    public class DemoChromelyApp : ChromelyBasicApp
+    {
+        public override void ConfigureServices(ServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            RegisterControllerAssembly(services, typeof(DemoChromelyApp).Assembly);
         }
     }
 }
--- BlazorDesk/Startup.cs
+++ BlazorDesk/Startup.cs
@@ -1,18 +1,52 @@
-﻿using BlazorComponents;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using WebWindows.Blazor;
+using Microsoft.Extensions.Hosting;
 
 namespace BlazorDesk
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        public void Configure(DesktopApplicationBuilder app)
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            app.AddComponent<App>("app");
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
         }
     }
 }

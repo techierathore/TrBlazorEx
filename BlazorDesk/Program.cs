70e26d88360247e1282e4353fe9d707350b06050
S Ravi Kumar <techierathore@gmail.com>
2021-06-12 11:00:49 +0530

    Implimented Photino in Desktop App

--- BlazorDesk/Program.cs
+++ BlazorDesk/Program.cs
@@ -1,90 +1,19 @@
-﻿using Chromely;
-using System.Threading;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using Chromely.Core.Configuration;
-using Chromely.Core;
+﻿using Photino.Blazor;
 using System;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Hosting;
 
 namespace BlazorDesk
 {
     public class Program
     {
-        public static void Main(string[] args)
+        [STAThread]
+        static void Main(string[] args)
         {
-            var appName = Assembly.GetEntryAssembly()?.GetName().Name;
-            var firstProcess = ServerAppUtil.IsMainProcess(args);
-            var port = ServerAppUtil.AvailablePort;
-
-            if (firstProcess)
-            {
-                if (port != -1)
-                {
-                    // start the kestrel server in a background thread
-                    var blazorTask = new Task(() => CreateHostBuilder(args, port).Build().Run(), TaskCreationOptions.LongRunning);
-                    blazorTask.Start();
-
-                    // wait till its up
-                    while (ServerAppUtil.IsPortAvailable(port))
-                    {
-                        Thread.Sleep(1);
-                    }
-                }
-
-                // Save port for later use by chromely processes
-                ServerAppUtil.SavePort(appName, port);
-            }
-            else
-            {
-                // fetch port number
-                port = ServerAppUtil.GetSavedPort(appName);
-            }
-
-            if (port != -1)
-            {
-                // start up chromely
-                var core = typeof(IChromelyConfiguration).Assembly;
-                var config = DefaultConfiguration.CreateForRuntimePlatform();
-                config.WindowOptions.Title = "blazor server app demo";
-                config.StartUrl = $"https://127.0.0.1:{port}";
-                config.DebuggingMode = true;
-                config.WindowOptions.RelativePathToIconFile = "chromely.ico";
-
-                try
-                {
-                    var builder = AppBuilder.Create();
-                    builder = builder.UseConfig<DefaultConfiguration>(config);
-                    builder = builder.UseApp<DemoChromelyApp>();
-                    builder = builder.Build();
-                    builder.Run(args);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    throw;
-                }
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args, int port) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                    .UseStartup<Startup>()
-                    .UseUrls(new[] { $"https://127.0.0.1:{port}" });
-                });
-    }
-
-    public class DemoChromelyApp : ChromelyBasicApp
-    {
-        public override void ConfigureServices(ServiceCollection services)
-        {
-            base.ConfigureServices(services);
-            RegisterControllerAssembly(services, typeof(DemoChromelyApp).Assembly);
+            ComponentsDesktop.Run<Startup>("Hello Photino Blazor!"
+                , "wwwroot/index.html"
+                , x: 450
+                , y: 100
+                , width: 1000
+                , height: 900);
         }
     }
 }
